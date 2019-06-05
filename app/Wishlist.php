be170432782faaf197d00a5ceedbd32685fe028c
Brijesh Pandey <brijesh@technocratshorizons.com>
2019-06-05 19:53:10 +0530

    apis

---
+++ app/Wishlist.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wishlist extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'created_at',
+        'gmail_id',
+        'timezone'
+    ];
+
+}
